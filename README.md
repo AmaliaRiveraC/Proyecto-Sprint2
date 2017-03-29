b589e71f34f3818290a52505e09900d31656b980
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-03-29 12:42:32 +0000

    probando otro camino para crear lista

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ProyectoFinal-Sprint2
-Hacer una imitación de la página Trello.
-Primero haremos un archivo html con estructura básica. Al cuál le pondremos una cabecera donde colocaremos una etiqueta h1 nuestro encabezado más importante.
-Crearemos una etiqueta section dentro de la cual haremos una etiqueta ul y su respectiva etiqueta li.
+Hacer una imitación del funcionamiento de la página Trello.
+Primero haremos un archivo html con estructura básica. Al cuál le pondremos una cabecera <header> donde colocaremos una etiqueta<h1> : nuestro encabezado más importante.
+Crearemos una etiqueta <section> dentro de la cual haremos una etiqueta <ul> y su respectiva etiqueta <li>.
 Tendremos que crear dos botones los cuales se encargaran de contener la función de crear lista y crear pendiente.
+En nuestro archivo javaScript que enlazaremos a nuestro html por medio de un script comenzaremos creando la función crearLista la cual se encargara de crear una lista en nuestro y podremos con la correspondiente función crearPendiente, es decir nuestro elemento li dentro de la lista, al cual se le otorgaran los valores que el usurio ingrese en el input.
+Crearemos también una función que se encargara de eliminar cada uno de nuetros pendientes.
--- assets/JS/sprint2.js
+++ assets/JS/sprint2.js
@@ -2,37 +2,46 @@
 
 function crearLista() {
     /*Esta funcion crea una nueva lista otorgandole al tiutlo el valor que el usuario a ingresado*/
-    var listaPendientes = document.getElementById("listaPendientes");
+    /*var listaPendientes = document.getElementById("listaPendientes");
     var listaCreada = document.createElement("ul");
     listaCreada.id = "nuevaLista";
     listaCreada = document.getElementById("nombreLista");
     listaCreada.innerHTML = document.getElementById("listaUsuario").value;
-    listaPendientes.appendChild(listaCreada);
+    listaPendientes.appendChild(listaCreada);*/
+    var listaNodo = document.getElementById("listaPendientes");
+    var nuevaLista = listaNodo.cloneNode(true);
+    nuevaLista = document.getElementById("nombreLista");
+    var listaUsuario = document.getElementById("listaUsuario");
+    nuevaLista.innerHTML = listaUsuario.value;
+    listaNodo.appendChild(nuevaLista);
+    nuevaLista.id = new Date();
+    listaNodo.insertBefore(nuevaLista, listaNodo.firstChild);
+    console.log(listaNodo);
+    listaUsuario.value = " ";
+    
+    console.log(nuevaLista);
 }
 
 function crearPendiente() {
+    //Esta funcion crea un nuevo pendiente dentro de nuestra lista
     var pendientes = document.getElementById("pendientes");
     var pendienteCreado = document.createElement("li");
     pendienteCreado.id = "nuevoPendiente";
     var pendientesUsuario = document.getElementById("pendienteUsuario");
-    pendienteCreado.innerHTML = pendientesUsuario.value;
+    pendienteCreado.innerHTML = pendientesUsuario.value + " " + "<span onclick='eliminar(this)'>X </span>";
     pendientes.appendChild(pendienteCreado);
     pendientesUsuario. value = " ";
 }
     
-   /* var titulo = document.createTextNode(tiutloLista);
-    listaUsuario.appendChild();
-    
-    tituloLista = espacioTitulo.value;
-    console.log(tituloLista);
-    
-    console.log(listaCreada);
+function eliminar(elemento) {
+    /*Esta funcion consigue eliminar los pendientes de manera independiente*/
+    var id=elemento.parentNode.getAttribute("id");
+    node=document.getElementById(id);
+    node.parentNode.removeChild(node);
+}
 
+function Lista = {
+    this.titulo = titulo;
+    this.input = document.getElementById("pendienteUsuario");
+}
 
-
-function crearTitulo() {
-    /*Esta funcion pretende tomar el valor del input donde se debria ingresar el titulo de la lista. Y crear un elemento lista utilizando dicho valor
-    var espacioTitulo = document.getElementById("nombreLista");
-    var titulo = document.getElementById("")
-    }
-*/
